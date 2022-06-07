d9d26ed1f892956eaf3e248554ab9cfeb7e18a21
Jiri Zraly <jirka@blueboard.cz>
2022-06-07 14:28:56 +0200

    build

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 export {default as multiselect} from  './components/multiselect/multiselect.js';
 export {default as modal} from  './components/modal/modal.js';
 export {default as dropdown} from  './components/dropdown/dropdown.js';
+
+window.Mistral = {
+    multiselect,
+    modal,
+    dropdown,
+}
