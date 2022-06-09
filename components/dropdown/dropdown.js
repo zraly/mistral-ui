1a4f7f5a51ad98eb7c05e0b0c6eb03ba32f8020a
Jiri Zraly <jirka@blueboard.cz>
2022-06-09 11:02:12 +0200

    Dropdown: Icon removed from component, added to examples

--- components/dropdown/dropdown.js
+++ components/dropdown/dropdown.js
@@ -7,12 +7,6 @@ export default () => ({
         "absolute bg-white rounded-lg shadow-lg py-3 w-max -translate-y-full -mt-3 top-0"
     },
     menuItem: "px-4 py-2 block outline-0 focus:bg-neutral-100 hover:bg-neutral-100 whitespace-nowrap",
-    icon: {
-      bottom:
-        '<svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 inline ml-1" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path  stroke-linecap="round" stroke-width="2" d="M5 10l7 7 7-7"/></svg>',
-      top:
-        '<svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" viewBox="0 0 24 24" class="w-5 h-5 inline ml-1"><path xmlns="http://www.w3.org/2000/svg" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 14l7-7 7 7"/></svg>'
-    }
   },
   position: "bottom",
   open: false,
@@ -22,7 +16,6 @@ export default () => ({
     this.position = wrapper.dataset.position === "top" ? "top" : "bottom";
     wrapper.className = style.wrapper;
     const triggerElement = this.$el.querySelector("[x-bind=trigger]");
-    triggerElement.innerHTML += style.icon[this.position];
     const menuElement = this.$el.querySelector("[x-bind=menu]");
     menuElement.className = style.menu[this.position];
     const menuItems = this.$el.querySelectorAll("[x-bind=menu] > li > a");
